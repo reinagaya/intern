14566326eaed78c8d514cfbca4511272e4897a8e
reinagaya <rei.nagaya.00@gmail.com>
2018-08-03 22:17:10 +0900

    [add]GETで送られてきたデータを確認のために表示

---
+++ registration/registration.php
@@ -0,0 +1,16 @@
+<p>あなたの登録した商品は，
+<br>
+<?php
+  // 送られてきたデータを連想配列に格納
+  $data = array("name" => $_GET["name"],
+                "price" => $_GET["price"],
+                "photo" => $_GET["photo"],
+                "text" => $_GET["text"]);
+
+  // 確認用に表示
+  print $data["name"].'<br>';
+  print $data["price"].'<br>';
+  print "<IMG SRC = ".$data["photo"].">".'<br>';
+  print $data["text"];
+?>
+</p>
